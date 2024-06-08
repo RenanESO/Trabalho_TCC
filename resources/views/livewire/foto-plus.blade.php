e02befa43ee3a79cceff7a8ee134b43bc0c231bb
RenanESO <renan.evilasio.97@gmail.com>
2024-06-08 19:34:57 -0300

    Mudança no visual do site CSS

--- resources/views/livewire/foto-plus.blade.php
+++ resources/views/livewire/foto-plus.blade.php
@@ -2,121 +2,160 @@
     
     <!-- Inicio :: Main -->
     <main>
-        <div id="main-slider" class="container-fluid mx-auto py-4 bg-white text-black" >
-            <div class="container m-auto py-5">
-                <div class='container-sm' style="margin-right:20rem;">
-                    <div class="row gx-5">
-                        <div class="col-lg">
-                            <div class="p-3">
-                                <h1>
-                                    Repositorio <br> Otimizado
-                                </h1>
+
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            </div>
+
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <div id="main-slider" class="container-fluid bg-white text-black">
+                        <div class='container-sm'>
+                            <div class="row gx-5">
+                                <div class="col-lg">
+                                    <div class="p-3">
+                                        <h1>
+                                            Repositório <br> Otimizado
+                                        </h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row gx-5">
+                                <div class="col-lg-6">
+                                    <div class="p-3">
+                                        <h3>
+                                            Resolva seus problemas de organização com os repositórios de fotos, 
+                                            utilizando a inteligência do FotoPlus. Organize fotos por datas, 
+                                            locais, pessoas e outros filtros.
+                                        </h3>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-3">
+                                        <!-- Coluna Vazia -->   
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row gx-5">
+                                <div class="col-lg">
+                                    <div class="p-3">
+                                        <a href="{{ asset('login') }}" class="btn btn-success">Confira Agora</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row gx-5">
-                        <div class="col-lg">
-                            <div class="p-3">
-                                <h3>
-                                    Resolva seus problemas de organização com os repositorios de fotos, 
-                                    utilizando a inteligência do FotoPlus. Organize fotos por datas, 
-                                    locais, pessoas e outros filtros.
-                                </h3>
+                    </div>   
+                </div>
+
+                <div class="carousel-item">
+                    <div id="servico" class="container-fluid bg-white text-black">
+                        <h1 class="text-center mb-4">Serviços</h1>
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+                            <div class="col">
+                                <div class="card h-100 m-2">
+                                    <img src="{{ asset('imagens/imgIA.png') }}" class="card-img-top" alt="card-RedeNeural">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Inteligência</h5>
+                                        <p class="card-text">
+                                            Utilizamos os principais metódos para reconhecer padrões em dados, 
+                                            especialmente em imagens, como Histogram of Oriented Gradients - HOG 
+                                            e Rede Neural Convolucional - CNN.
+                                        </p>
+                                    </div>
+                                    <div class="card-footer">
+                                        <small class="text-muted">FotoPlus é Eficiente</small>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg">
-                            <div class="p-3">
-                                <!-- Coluna Vazia -->   
+                            <div class="col">
+                                <div class="card h-100 m-2">
+                                    <img src="{{ asset('imagens/imgSeguranca.png') }}" class="card-img-top" alt="card-Seguranca">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Segurança</h5>
+                                        <p class="card-text">
+                                            Sinta-se seguro utilizando nossos serviços, oferecendo 
+                                            um alto nível de segurança para sua imagens e dados pessoais.                        
+                                        </p>
+                                    </div>
+                                    <div class="card-footer">
+                                        <small class="text-muted">FotoPlus é Confiabilidade</small>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row gx-5">
-                        <div class="col-lg">
-                            <div class="p-3">
-                                <a href="{{ asset('login') }}" class="btn btn-success">Confira Agora</a>
+                            <div class="col">
+                                <div class="card h-100 m-2">
+                                    <img src="{{ asset('imagens/imgOrganizacao.png') }}" class="card-img-top" alt="card-Organizacao">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Organização</h5>
+                                        <p class="card-text">
+                                            Tenha uma experiência melhorada em gerenciar suas imagens 
+                                            no Google Drive. Filtre fotos pela data e pessoa 
+                                            específica, além disso pode veificar fotos duplicadas.
+                                        </p>
+                                    </div>
+                                    <div class="card-footer">
+                                        <small class="text-muted">FotoPlus é Inovador</small>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </div>          
                     </div>
                 </div>
             </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden"> Anterior </span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden"> Próximo </span>
+            </button>
+
         </div>
 
-        <div id="servico" class="container-fluid mx-auto py-4">
-            <div class="container m-auto">
-                <h1 class="text-center">Serviços</h1>
-                <div class="row row-cols-1 row-cols-md-3 g-4 py-4">
-                    <div class="col">
-                        <div class="card h-100">
-                            <img src="{{ asset('imagens/imgIA.png') }}" class="card-img-top" alt="card-RedeNeural">
-                            <div class="card-body">
-                                <h5 class="card-title">Redes Neurais CNN</h5>
-                                <p class="card-text">
-                                    Uma Rede Neural Convolucional (Convolutional Neural 
-                                    Network - CNN) é um tipo de algoritmo de 
-                                    aprendizado de máquina que pode ser usado para reconhecer 
-                                    padrões em dados, especialmente em imagens.
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">FotoPlus é Eficiente</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                            <img src="{{ asset('imagens/imgSeguranca.png') }}" class="card-img-top" alt="card-Seguranca">
-                            <div class="card-body">
-                                <h5 class="card-title">Segurança</h5>
-                                <p class="card-text">
-                                    Sinta-se seguro utilizando nossos serviços, oferecendo 
-                                    um alto nivel de segurança para os dados.
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">FotoPlus é Confiabilidade</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                            <img src="{{ asset('imagens/imgOrganizacao.png') }}" class="card-img-top" alt="card-Organizacao">
-                            <div class="card-body">
-                                <h5 class="card-title">Organização</h5>
-                                <p class="card-text">
-                                    Tenha uma esperiencia melhorada em gerenciar suas fotos 
-                                    nos principais armazenamentos como: Google Drive e OneDrive. 
-                                    Filtre fotos pela data e local, podendo eliminar fotos 
-                                    duplicadas e buscar fotos de pessoas espeficas.
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">FotoPlus é Inovador</small>
-                            </div>
+        <div id="sobre" class="container-fluid" >
+            <h1 class="text-center mb-4">Sobre Nós</h1>
+            <div  class="row align-items-center">
+                <div class="col">
+                    <div class="card">
+                        <a href="https://www.formiga.ifmg.edu.br">
+                            <img id="image-instituicao" src="{{ asset('imagens/IFMG.png') }}" alt="imagem-campus">
+                        </a>    
+                        <div class="card-body">
+                            <p class="card-text fs-5">
+                                A FotoPlus foi concebida como parte do Trabalho de Conclusão de Curso (TCC) 
+                                do aluno Renan Evilásio Silva de Oliveira, estudante do curso de Ciência da 
+                                Computação do Instituto Federal de Minas Gerais, campus Formiga. O projeto 
+                                visa otimizar e facilitar a organização e a busca de fotografias armazenadas. 
+                                A iniciativa surge da necessidade de uma solução eficiente para a gestão de 
+                                imagens, proporcionando aos usuários uma ferramenta que simplifique o processo 
+                                de categorização e localização de fotos em grandes volumes de armazenamento. 
+                                Este projeto reflete o compromisso acadêmico e a inovação tecnológica no 
+                                desenvolvimento de soluções práticas para desafios contemporâneos.
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    
-        <div id="sobre" class="container-fluid mx-auto py-4" >
-            <div class="container m-auto">
-                <h1 class="text-center">Sobre Nós</h1>
-                <div id="img-campus" class="row align-items-center">
-                    <div class="col">
-                        <div class="card border-0 h-100 py-4">
-                            <a href="https://www.formiga.ifmg.edu.br">
-                                <img src="{{ asset('imagens/IFMG.png') }}" alt="img-campus">
-                            </a>                      
-                            <div class="card-body">
-                                <h2 class="card-text py-3">
-                                    FotoPlus teve iniciativa pelo projeto de TCC do aluno 
-                                    Renan Evilásio Silva de Oliveira, bacharelando do curso Ciência da Computação 
-                                    do Instituto Federal de Minas Gerais campus Formiga. Com o 
-                                    objetivo de otimizar e facilitar a organização e busca de 
-                                    fotografias contidas em armazenamentos.
-                                </h2>
-                            </div>
-                        </div>
+            <br>
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <p id="descricao-aluno" class="fs-6">
+                        "Olá, pessoal! Meu nome é Renan Evilásio, entusiasta por tecnologia e possuo mais 
+                        de 5 anos de experiência em Delphi. Também tenho conhecimentos intermediários em 
+                        Python e iniciante em Laravel. Acredito que a organização do código é fundamental 
+                        para a manutenção e compreensão do mesmo. Além disso, estou sempre disposto a 
+                        aprender novas tecnologias para acompanhar a constante evolução da área."
+                    </p>
+                </div>
+                <div class="col-md-6 text-center">
+                    <div class="image-container">
+                        <img id="image-aluno" src="{{ asset('imagens\Renan.2.1.jpg') }}" alt="Sua Imagem" class="img-fluid rounded-circle">
                     </div>
                 </div>
             </div>
